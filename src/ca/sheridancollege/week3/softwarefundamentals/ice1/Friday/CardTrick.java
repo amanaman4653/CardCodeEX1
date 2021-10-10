3488511036b729062b1144b73d2b0e65bde4d8d4
amanaman4653aman0@sheridancollege.ca <>
2021-10-09 22:33:44 -0400

    NeW Card Game Ex 1..2

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * modified by Aman Aman 
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+      System.out.println(); 
     }
     
 }
